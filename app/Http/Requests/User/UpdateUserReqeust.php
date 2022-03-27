2b33073beff20b3993feb34e025534782493bd80
andikarafon <andika.rafon.senohaji@gmail.com>
2022-03-27 14:45:23 +0700

    data comit

---
+++ app/Http/Requests/User/UpdateUserReqeust.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use App\Models\User;
+
+
+use Illuminate\Foundation\Http\FormRequest;
+use Symfony\Component\HttpFoundation\Response;
+
+
+
+
+class UpdateUserReqeust extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => [
+                'required', 'string','max:255',
+            ],
+            'email' => [
+                'required', 'email', 'max:255',  Rule::unique('users')->ignore($this->user), //rule works only for other record id
+            ],
+            'password' => [
+                'min:8', 'string', 'max:255', 'mixedcase',
+            ],
+        ];
+    }
+}

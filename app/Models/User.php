77e37c235bb81c14214e4aecb50c61e35ff03848
andikarafon <andika.rafon.senohaji@gmail.com>
2022-04-05 11:03:32 +0700

    update Livewire

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,36 +8,25 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     use HasApiTokens;
-    //use HasFactory;
+    use HasFactory;
     use HasProfilePhoto;
+    use HasTeams;
     use Notifiable;
     use TwoFactorAuthenticatable;
-    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var string[]
      */
-
-    protected $dates = [ 
-        'created_at', 
-        'updated_at',
-        'deleted_at',
-        'email_verified_at',
-    ];
- 
-
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'name', 'email', 'password',
     ];
 
     /**
@@ -69,22 +58,4 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
-
-    public function role_user()
-    {
-        return $this->hasMany('App\Models\ManagementAccess\RoleUser', 'user_id');
-    }
-
-    public function detail_user()
-    {
-        return $this->hasOne('App\Models\ManagementAccess\DetailUser', 'user_id');
-    }
-
-    public function appointment()
-    {
-        return $this->hasMany('App\Models\Operational\Appointment', 'user_id');
-    }
-
-
-
 }

58a7ee15878fa60c370ee84390b93eb43516b846
andikarafon <andika.rafon.senohaji@gmail.com>
2022-04-28 11:00:37 +0700

    Fix detail_user

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -4,6 +4,10 @@ namespace App\Actions\Fortify;
 
 use App\Models\Team;
 use App\Models\User;
+
+
+use App\Models\ManagementAccess\DetailUser;
+
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -36,6 +40,16 @@ class CreateNewUser implements CreatesNewUsers
                 'password' => Hash::make($input['password']),
             ]), function (User $user) {
                 $this->createTeam($user);
+
+                  //add to detail_user, di saat user dibuat maka detail user juga dibuat
+                  $detail_user = new DetailUser;
+                  $detail_user->user_id         = $user->id; //langsung mengiisi dari id_user
+                  $detail_user->type_user_id    = 3; //user baru terisi menjadi pasien
+                  $detail_user->contact         = NULL;
+                  $detail_user->address         = NULL;
+                  $detail_user->photo           = NULL;
+                  $detail_user->gender          = NULL;
+                  $detail_user->save();
 
             });
         });
